49567b7ad9ac5c8db2cda87fe64442870b5df101
yuragms <yura.gms@gmail.com>
2024-01-29 13:04:02 +0200

    init

--- main.js
+++ main.js
@@ -1,15 +1,24 @@
 import { Telegraf, Markup } from 'telegraf';
-const webAppUrl = 'https://google.com/';
+import { message } from 'telegraf/filters';
 
 const token = '6740251900:AAERSd4oIjaVkQKiD9tBS-Lr2IvXRDXUlFs';
+
+const webAppUrl = 'https://angular-tg-app2.web.app';
 
 const bot = new Telegraf(token);
 
 bot.command('start', (ctx) => {
   ctx.reply(
     'Добро пожаловать! Нажмите кнопку ниже, чтобы запустить приложение',
-    Markup.keyboard([Markup.button.webApp('Отправить сообщение', webAppUrl)])
+    Markup.keyboard([
+      Markup.button.webApp('Отправить сообщение', `${webAppUrl}/feedback`),
+    ])
   );
+});
+
+bot.on(message('web_app_data'), async (ctx) => {
+  const data = ctx.webAppData.data.json();
+  ctx.reply(`Ваше сообщение: ${data?.feedback}` ?? 'empty message');
 });
 
 bot.launch();
